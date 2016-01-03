5cf5a3bc4529748d94d77f1f13c91b3ae695087a
Jovi Chokor <jomonavi@gmail.com>
2016-01-03 17:21:03 -0500

    committing changes

--- public/js/nodesFactory.js
+++ public/js/nodesFactory.js
@@ -1,65 +1,112 @@
-app.factory('NodesFactory', function($http){
+app.factory('NodesFactory', function($http, $sce){
+
+	function _handleError(response) {
+	  // TODO: Do something here. Probably just redirect to error page
+	  console.log('%c ' + response, 'color:red'); 
+	}
+
+	Array.prototype.getIndexBy = function (name, value) {
+	    for (var i = 0; i < this.length; i++) {
+	        if (this[i][name] == value) {
+	            return i;
+	        }
+	    }
+	    return -1;
+	}
+
 	return {
-		getNodes = function(payload){
-
-			$http.post('/home', {artist: payload}).then(function(allNodes){
-			if(!Array.isArray(allNodes.data)){
-				alert("Check the spelling on your search!");
-			}
-			var nodeArr = [], linkArr = [], sliceStart;
-			allNodes.data.forEach(function(node, idx){
-				node.level = "core" + idx, node.isParent = true;		
-				node.samplesCollection.forEach(function(aNode){
-	        		aNode.level = idx; 
-	        	});
-				nodeArr.push(node);
-				sliceStart = nodeArr.length;
-
-				Array.prototype.splice.apply(nodeArr, [sliceStart, 0].concat(node.samplesCollection));
-				for(var i = sliceStart; i < nodeArr.length; i++) {
-					var aLink = {
-						'source': sliceStart - 1,
-						'target': i,
-						'weight': 3
-					}
-					linkArr.push(aLink);
+		getNodes: function(payload){
+			$('#loading').show();
+			var artistQ = payload.artist.split(" ").join("-");
+			$http.post('/home', {artist: artistQ, page: payload.page}).then(function(allNodes){
+				if(!Array.isArray(allNodes.data)){
+					alert("Check the spelling on your search!");
 				}
-			});
 
-			var nodes = nodeArr;
-			var links = linkArr;
+				var nodeArr = [], linkArr = [], sliceStart;
+				allNodes.data.forEach(function(node, idx){
+					node.level = "core" + idx, node.isParent = true;		
+					node.samplesCollection.forEach(function(aNode, idex){
+		        		aNode.level = idx;
+		        		aNode.branchRef = "branch" + idex;
+		        		aNode.otherSamplers.forEach(function(branch, i){
+		        			branch.original = aNode.songName;
+		        			branch.leafLevel = idx;
+		        			if(branch.songName === node.songName){
+		        		 		aNode.otherSamplers.splice(i, 1);
+		        			}
+		        		 })
+		        	});
+					nodeArr.push(node);
+					sliceStart = nodeArr.length;
 
-			var width = 823, height = 600;
+					Array.prototype.splice.apply(nodeArr, [sliceStart, 0].concat(node.samplesCollection));
 
-			var color = d3.scale.category20();
+					for(var i = sliceStart; i < nodeArr.length; i++) {
+						var aLink = {
+							'source': sliceStart - 1,
+							'target': i,
+							'weight': 3
+						}
+						linkArr.push(aLink);
+					}
+					node.samplesCollection.forEach(function(sampleNode){
+						console.log(nodeArr.length, sampleNode.otherSamplers);
+						Array.prototype.splice.apply(nodeArr, [nodeArr.length, 0].concat(sampleNode.otherSamplers));
+					});
+				});
 
-			var fisheye = d3.fisheye.circular()
-				.radius(120);
+				nodeArr.forEach(function(node, idx){
+					console.log("it's failing here? node.orig is here", node.original);
+					if(node.original){
+						console.log("another thing", node.original);
+						var sourcePos = nodeArr.getIndexBy("songName", node.original);
 
-			var force = d3.layout.force()
-				.charge(-150)
-				.linkDistance(35)
-				.size([width, height]);
+						var aLink = {
+							'source': sourcePos,
+							'target': idx,
+							'weight': 3
+						}
+						linkArr.push(aLink);
+					}
+				})
 
-			  var svg = d3.select("#svg-canvas").append("svg")
-			      .attr("width", width)
-			      .attr("height", height);
+				var nodes = nodeArr;
+				var links = linkArr;
 
-			  svg.append("rect")
-			      .attr("class", "background")
-			      .attr("width", width)
-			      .attr("height", height);
+				var width = 823, height = 600;
 
-			    var n = nodes.length;
+				var color = d3.scale.category20();
+
+				var fisheye = d3.fisheye.circular()
+					.radius(120);
+
+				var force = d3.layout.force()
+					.charge(-150)
+					.linkDistance(35)
+					.size([width, height]);
+
+				$('#loading').hide();
+
+				var svg = d3.select("#svg-canvas").append("svg")
+					.attr("width", width)
+					.attr("height", height);
+
+				svg.append("rect")
+					.attr("class", "background")
+					.attr("width", width)
+					.attr("height", height);
+
+				var n = nodes.length;
 
 			    force.nodes(nodes).links(links);
 
-			    // Initialize the positions deterministically, for better results.
-			    // nodes.forEach(function(d, i) { d.x = d.y = width / n * i; });
+				// Initialize the positions deterministically, for better results.
+				// nodes.forEach(function(d, i) { d.x = d.y = width / n * i; });
 
-			    // Run the layout a fixed number of times.
-			    // The ideal number of times scales with graph complexity.
-			    // Of course, don't run too long—you'll hang the page!
+				// Run the layout a fixed number of times.
+				// The ideal number of times scales with graph complexity.
+				// Of course, don't run too long—you'll hang the page!
 			    force.start();
 			    for (var i = n; i > 0; --i) force.tick();
 			    force.stop();
@@ -95,14 +142,14 @@ app.factory('NodesFactory', function($http){
 			        .style("fill", function(d) { return "gold"; })
 			        .call(force.drag);
 
-			        $('svg circle').tipsy({ 
-				        gravity: 'w', 
-				        html: true, 
-				        title: function() {
-				          var d = this.__data__, songName = d.songName, artist = d.artistName;
-				          return songName + " by " + artist;
-				        }
-				      });
+		        $('svg circle').tipsy({ 
+			        gravity: 'w', 
+			        html: true, 
+			        title: function() {
+			          var d = this.__data__, songName = d.songName, artist = d.artistName;
+			          return songName + " by " + artist;
+			        }
+			      });
 
 		        node.on("click", function(d) {
 		        	if(d.songLink.indexOf("https") === -1){
@@ -172,9 +219,10 @@ app.factory('NodesFactory', function($http){
 
 			    $("#navigation").show();	
 
-		}, function(error){
-			alert("Check the spelling of your search");
-			_handleError(error);
-		})
+			}, function(error){
+				alert("Check the spelling of your search");
+				_handleError(error);
+			})
+		}
 	}
-})
+});
