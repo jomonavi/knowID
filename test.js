55f7ad98165cef67559417262513c3fa67d6d1c5
Jovi Chokor <jomonavi@gmail.com>
2016-01-24 23:42:33 -0800

    deleted a file

--- test.js
+++
@@ -1,8 +0,0 @@
-var expect = require('chai').expect;
-var scraper = require('./routes/nodes.js');
-
-describe('simple-test', function(){
-	it('should work', function(){
-		expect(true).to.be.true; 
-	});
-})
